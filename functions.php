4d2b5fe4b3cb62fa160e877bb2a22030de51787c
Marco Aspeitia <maspeitia.31@gmail.com>
2020-08-05 10:41:34 -0500

    Esential functions in the theme, styles, js and theme setup

--- functions.php
+++ functions.php
@@ -25,3 +25,20 @@ function menuqr_devitm_setup() {
 }
 endif; // menuqr_devitm_setup
 add_action( 'after_setup_theme', 'menuqr_devitm_setup' );
+
+/**
+ * Proper way to enqueue scripts and styles
+ */
+function menuqr_devitm_theme_scripts() {
+    /* Styles */
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '4.5.0');
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', false, '4.7.0');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap-css','font-awesome') );
+
+    /* Scripts */
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.min.js', array('jquery'), '2.4.0', true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('popper'), '4.5.0', true);
+
+}
+add_action( 'wp_enqueue_scripts', 'menuqr_devitm_theme_scripts' );
